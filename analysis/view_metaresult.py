4d1823504339655c5b8cb46b85401c4f109c0281
casszhao <zhixue.zhao@sheffield.ac.uk>
2022-09-11 17:51:23 +0100

    m

--- analysis/view_metaresult.py
+++ analysis/view_metaresult.py
@@ -7,11 +7,13 @@ from sklearn.metrics import accuracy_score
 
 #### check accuracy and sufficiency...
 
-dataset = 'evinf_top'
-result = np.load(f'./trained_models/{dataset}/scibert-output_seed-sci15.npy', allow_pickle= True)
-meta = f'./faithfulness_metrics/{dataset}/topk-test-faithfulness-metrics.json'
-get_map = pd.read_csv(f'./datasets/{dataset}/data/test.csv')
+dataset = 'evinf'
+rationale_type_dataset = str(dataset) + '_top'
+result = np.load(f'./trained_models/{rationale_type_dataset}/scibert-output_seed-sci15.npy', allow_pickle= True)
+meta_top = f'./faithfulness_metrics/{dataset}/topk-test-faithfulness-metrics.json'
+meta_conti = f'./faithfulness_metrics/{dataset}/contigious-test-faithfulness-metrics.json'
 
+get_map = pd.read_csv(f'./datasets/{rationale_type_dataset}/data/test.csv')
 label_num = get_map['label'].value_counts().to_frame()
 label_num = label_num.index.to_list()
 feat_num = get_map['feat'].value_counts().to_frame()
@@ -27,7 +29,8 @@ for key in label_num:
 
 print(map)
 
-with open(meta, "r") as file : prediction_data = json.load(file)
+with open(meta_top, "r") as file : prediction_data_top = json.load(file)
+with open(meta_conti, "r") as file : prediction_data_conti = json.load(file)
 
 df = pd.DataFrame.from_dict(result.item(0)).T
 print(df[:5])
@@ -48,30 +51,62 @@ print('accuracy: ', accuracy_score(list(df['actual']), pred_labels))
 
 print('-----------')
 
-suff_list = []
-comp_list = []
-best_suff_list = []
-best_comp_list = []
+suff_list_top = []
+comp_list_top = []
+best_suff_list_top = []
+best_comp_list_top = []
 
-for id in prediction_data.keys():
+for id in prediction_data_top.keys():
     label_num = df.loc[id]['pred_labels']
     label_feat = 'fixed-' + str(map.get(label_num))
 
-    suff = prediction_data.get(str(id)).get(label_feat).get('sufficiency')
-    comp = prediction_data.get(str(id)).get(label_feat).get('comprehensiveness')
-    best_suff = prediction_data.get(str(id)).get('var-var-len_var-feat').get('sufficiency')
-    best_comp = prediction_data.get(str(id)).get('var-var-len_var-feat').get('comprehensiveness')
-    suff_list.append(suff)
-    comp_list.append(comp)
-    best_suff_list.append(best_suff)
-    best_comp_list.append(best_comp)
+    suff_top = prediction_data_top.get(str(id)).get(label_feat).get('sufficiency')
+    comp_top = prediction_data_top.get(str(id)).get(label_feat).get('comprehensiveness')
+    best_suff_top = prediction_data_top.get(str(id)).get('var-var-len_var-feat').get('sufficiency')
+    best_comp_top = prediction_data_top.get(str(id)).get('var-var-len_var-feat').get('comprehensiveness')
+    suff_list_top.append(suff_top)
+    comp_list_top.append(comp_top)
+    best_suff_list_top.append(best_suff_top)
+    best_comp_list_top.append(best_comp_top)
 
+print('--- top ----')
+print('--- classifier ----suff-->comp')
+print(sum(suff_list_top)/len(suff_list_top))
+print(sum(comp_list_top)/len(comp_list_top))
 
-print(sum(suff_list)/len(suff_list))
-print(sum(comp_list)/len(comp_list))
-print(sum(best_suff_list)/len(best_suff_list))
-print(sum(best_comp_list)/len(best_comp_list))
+# print(sum(best_suff_list_top)/len(best_suff_list_top))
+# print(sum(best_comp_list_top)/len(best_comp_list_top))
 
-print(prediction_data.get('3253463_0').get('fixed-deeplift').get('comprehensiveness'))
+'''
+print(prediction_data_top.get('3253463_0').keys())
+dict_keys(['fixed-deeplift', 'fixed-lime', 'fixed-attention', 'fixed-ig', 'fixed-gradients', 'fixed-scaled attention', 'fixed-random', 
+'fixed-fixed-len_var-feat', 'fixed-fixed-len_var-feat_var-type', 
+              'var-deeplift', 'var-lime', 'var-attention', 'var-ig', 'var-gradients', 'var-scaled attention', 'var-random', 
+'var-var-len_var-feat', 'var-var-len_var-feat_var-type']
+'''
 
+suff_list_conti = []
+comp_list_conti = []
+best_suff_list_conti = []
+best_comp_list_conti = []
 
+for id in prediction_data_conti.keys():
+    label_num = df.loc[id]['pred_labels']
+    label_feat = 'fixed-' + str(map.get(label_num))
+
+    suff_conti = prediction_data_conti.get(str(id)).get(label_feat).get('sufficiency')
+    comp_conti = prediction_data_conti.get(str(id)).get(label_feat).get('comprehensiveness')
+    best_suff_conti = prediction_data_conti.get(str(id)).get('var-var-len_var-feat').get('sufficiency')
+    best_comp_conti = prediction_data_conti.get(str(id)).get('var-var-len_var-feat').get('comprehensiveness')
+    suff_list_conti.append(suff_conti)
+    comp_list_conti.append(comp_conti)
+    best_suff_list_conti.append(best_suff_conti)
+    best_comp_list_conti.append(best_comp_conti)
+
+print('--- conti ----')
+print('--- classifier ----suff-->comp')
+print(sum(suff_list_conti)/len(suff_list_conti))
+print(sum(comp_list_conti)/len(comp_list_conti))
+# print('--- georgy ----')
+# print(sum(best_suff_list_conti)/len(best_suff_list_conti))
+# print(sum(best_comp_list_conti)/len(best_comp_list_conti))
--- datasets/multirc/data/download_from_huggingface.py
+++ datasets/multirc/data/download_from_huggingface.py
@@ -1,11 +1,22 @@
 from datasets import load_dataset
-
+import pandas as pd
 #train = load_dataset('super_glue', 'multirc', split='train')
 #test = load_dataset('super_glue', 'multirc', split='test')
 # dev = load_dataset('super_glue', 'multirc', split='validation')
 # print(dev[0])
-import panda as pd
 
-df = pd.read_csv('dev.csv')
-df["label"] = df["label"].map({"false": 0, "true": 1})
-df.to_csv('de.csv')
+
+df = pd.read_csv('train.csv')
+print(df.head())
+
+list = []
+for l in df["label"]:
+    if str(l) == "False":
+        list.append(0)
+    elif str(l) == "True": 
+        list.append(1)
+
+
+df['label'] = list
+df.to_csv('train.csv')
+print(df.head())
