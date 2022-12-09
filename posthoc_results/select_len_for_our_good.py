5f56249c3b5edbb216dbe006f08aa78138f7b869
casszhao <zhixue.zhao@sheffield.ac.uk>
2022-12-09 22:03:33 +0000

    update with jade

--- posthoc_results/select_len_for_our_good.py
+++ posthoc_results/select_len_for_our_good.py
@@ -6,7 +6,7 @@ feat_attr_list = ["attention", "scaled attention", "ig", "gradients", "deeplift"
 
 
 
-method = 'ZEROOUTlimit'
+method = 'NOISElimit'
 dataset = 'sst'
 rationale_ratios = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5]
 
