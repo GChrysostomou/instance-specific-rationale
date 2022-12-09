6692f68981d8589ac8f44d1e355dcae7fcdaddc7
Cass Zhao <cass@dcs33421.local>
2022-12-09 22:52:47 +0000

    :@

--- posthoc_results/select_len_for_our_good.py
+++ posthoc_results/select_len_for_our_good.py
@@ -69,10 +69,7 @@ def compare_save_json(dataset, method, rationale_ratios):
 # get_descriptor(dataset, method, [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0])
 # compare_save_json(dataset, method, rationale_ratios)
 
-
-
-
-method = 'NOISElimit'
+method = 'ZEROOUTlimit'
 dataset = 'multirc'
 
 rationale_ratios_different_list = [
